cc2040d3d304d6d882f89b067e04b701580bc464
Hasan75 <hasanjab14@gmail.com>
2021-10-10 02:39:03 +0600

    firebase config added

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBuUkafx93xzvoZQyWO10c250-HHTu4Dt4',
+  authDomain: 'ema-john-ecommerce-auth.firebaseapp.com',
+  projectId: 'ema-john-ecommerce-auth',
+  storageBucket: 'ema-john-ecommerce-auth.appspot.com',
+  messagingSenderId: '530777620061',
+  appId: '1:530777620061:web:ec5afe84f898b902a717b4',
+};
+
+export default firebaseConfig;
