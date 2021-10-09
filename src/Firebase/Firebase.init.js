db39a1b133d61dcaee2a28aff57faec3cc484434
Hasan75 <hasanjab14@gmail.com>
2021-10-10 02:39:47 +0600

    added the steps in comment

---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './Firebase.config';
+
+const initializeAuthentication = () => {
+  initializeApp(firebaseConfig);
+};
+
+export default initializeAuthentication;
+
+/*
+steps for authentication
+----------------------
+1. firebase: create project
+2. create web app
+3. get configuration
+4. initialize firebase
+5. Enable auth method
+
+*/
