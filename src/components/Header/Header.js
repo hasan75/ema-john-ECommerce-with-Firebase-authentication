a4ff33c34b74bb2866bba63df73e0f67bf1ecb06
Hasan75 <hasanjab14@gmail.com>
2021-10-11 01:30:58 +0600

    login logout name dynamically added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
 import logo from '../../images/logo.png';
 import './Header.css';
+import useAuth from '../../hooks/useAuth';
 
 const Header = () => {
-  const { user, logOut } = useFirebase();
+  const { user, logOut } = useAuth();
+
   return (
     <div className='header'>
       <img className='logo' src={logo} alt='' />
@@ -13,6 +14,9 @@ const Header = () => {
         <NavLink to='/shop'>Shop</NavLink>
         <NavLink to='/review'>Order Review</NavLink>
         <NavLink to='/inventory'>Manage Inventory</NavLink>
+        {user.email && (
+          <span style={{ color: 'white' }}>Hello! {user.displayName} </span>
+        )}
         {user.email ? (
           <button onClick={logOut}> LogOut </button>
         ) : (
