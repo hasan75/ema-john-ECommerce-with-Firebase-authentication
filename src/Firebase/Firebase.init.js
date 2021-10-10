2c88657d0fbb9f7faf6e8c48a161e4f2ef720b41
Hasan75 <hasanjab14@gmail.com>
2021-10-11 01:59:55 +0600

    private route added

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -18,13 +18,13 @@ Step-1: Initial Setup
 4. initialize firebase
 5. Enable auth method
 
-Step-2:
+Step-2: set up compomnet
 1.Create Login Component
 2.Create Register Compoentn
 3.Create Route for Logic and Register
 
 ------------------
-Step 3:
+Step 3:set auth system
 1.set up sign in method
 2. setup sign out method
 3. user state
@@ -32,10 +32,15 @@ Step 3:
 5. return necessary methods and states from useFirebase
 
 -----------------
-Step 4:
+Step 4: create auth context hook (useAuth)
 1. create a auth contex
 2. create context provider
 3. set Context provider context value.
 4. use auth provider
 5.create useAuth hook
+
+-------------------
+Step 5: create private route
+1. create private route
+2.set private route
 */
